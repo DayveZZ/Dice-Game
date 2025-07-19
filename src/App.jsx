75003dbfdd0a899f8d19042afb09a5bf5018a43c
DayveZZ <abhijitdebofficial@gmail.com>
2025-07-19 12:25:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+// import StartGame from "./components/StartGame";
+
+function App() {
+  const hello1 = "hello";
+  console.log(hello1);
+
+  return (
+    <>
+      <div>hello</div>
+      <div>hello</div>
+      <div>hello</div>
+      <div>hello</div>
+    </>
+  );
+}
+export default App;
