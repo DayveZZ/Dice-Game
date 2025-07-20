f13526a3c77cce61b03c383322359b4277556c4b
DayveZZ <abhijitdebofficial@gmail.com>
2025-07-20 14:35:53 +0530

    fourth commit

--- src/components/Gameplay.jsx
+++ src/components/Gameplay.jsx
@@ -1,15 +1,62 @@
-function Gameplay() {
-  return (
-    <div className="flex items-center justify-evenly h-screen bg-gray-100">
-      <img src="src\assets\dices.png" className="w-2/5" />
+import { useState } from "react";
+import Rules from "./Rules";
 
-      <div className="flex flex-col items-end justify-between h-50">
-        <h1 className="text-9xl">DICE GAME</h1>
-        <button className="bg-black text-white text-2xl px-6 py-3 rounded-md cursor-pointer hover:bg-white hover:text-black hover:shadow-md transition duration-400">
-          Play Now
+function Gameplay() {
+  const [show, setShow] = useState(false);
+  return (
+    <div className=" h-screen bg-gray-100 p-10">
+      <div className="flex items-center justify-between h-1/5">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-5xl text-center mb-5">0</h1>
+          <span>Your Score</span>
+        </div>
+
+        <div className="text-end">
+          <ul className="selectNumber flex items-center justify-center space-x-4 mb-5">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+          </ul>
+          <h1 className="font-semibold">Select Number</h1>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center h-3/5">
+        <button>
+          <img src="src\assets\dice_1.png" />
         </button>
+        <h1 className="text-md">Click on the Dice to roll</h1>
+        <button className="w-50 border-2 border-black px-3 py-2 rounded-md cursor-pointer hover:bg-black hover:text-white transition duration-400 mt-5">
+          Reset Score
+        </button>
+        <div></div>
+      </div>
+
+      <div className="flex justify-between">
+        <button
+          className="w-50 h-12 border-2 border-black px-3 py-2 rounded-md cursor-pointer bg-black text-white hover:bg-white hover:text-black transition duration-400"
+          onClick={() => setShow(!show)}
+        >
+          Show Rules
+        </button>
+        {show && <Rules />}
       </div>
     </div>
   );
 }
 export default Gameplay;
+
+{
+  /* <h2>How to play dice game</h2>
+      <div className="text">
+        <p>Select any number</p>
+        <p>Click on dice image</p>
+        <p>
+          after click on dice if selected number is equal to dice number you
+          will get same point as dice{" "}
+        </p>
+        <p>if you get wrong guess then 2 point will be dedcuted </p> */
+}
---
+++ src/components/Rules.jsx
@@ -0,0 +1,17 @@
+function Rules() {
+  return (
+    <div className="w-4/5 bg-grey-200 shadow-lg rounded-md px-3 py-2">
+      <h2 className="font-bold uppercase">How to play dice game</h2>
+      <ul>
+        <li>1. Select any number</li>
+        <li>2. Click on dice image</li>
+        <li>
+          3. After click on dice if selected number is equal to dice number you
+          will get same point as dice
+        </li>
+        <li>4. if you get wrong guess then 2 point will be dedcuted</li>
+      </ul>
+    </div>
+  );
+}
+export default Rules;
